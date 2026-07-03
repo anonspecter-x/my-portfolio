866b168a6f8ce07e16fe10e955b7346047b07f15
anonspecter-x <hello.naz.sakib@gmail.com>
2026-07-03 22:10:21 +0600

    header upg 2.61

--- components/Header.tsx
+++ components/Header.tsx
@@ -149,9 +149,9 @@ export default function Header({ settings, tracks }: HeaderProps) {
     let initialScrollY = window.scrollY;
 
     const handleMobileScroll = () => {
-      // যদি ইউজার সত্যিই ২০ পিক্সেলের বেশি স্ক্রল করে, তবেই বন্ধ হবে।
+      // যদি ইউজার সত্যিই ৫ পিক্সেলের বেশি স্ক্রল করে, তবেই বন্ধ হবে।
       // এটি ব্রাউজারের অটোমেটিক ছোট স্ক্রলকে ইগনোর করবে।
-      if (Math.abs(window.scrollY - initialScrollY) > 20) {
+      if (Math.abs(window.scrollY - initialScrollY) > 1) {
         setIsMusicOpen(false);
       }
     };
@@ -590,4 +590,3 @@ export default function Header({ settings, tracks }: HeaderProps) {
       </AnimatePresence>
     </>
   );
-}
