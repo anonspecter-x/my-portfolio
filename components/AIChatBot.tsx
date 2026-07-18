170670172b5f805922a9c8982ee4b6ac8b27f19d
anonspecter-x <hello.naz.sakib@gmail.com>
2026-07-19 01:03:20 +0600

    chatbot upg (glass) v4.2

--- components/AIChatBot.tsx
+++ components/AIChatBot.tsx
@@ -152,6 +152,14 @@ export default function AIChatBot() {
     });
   };
 
+  // 🚀 Toggle Window state
+  const toggleWindow = () => {
+    if (!isOpen) {
+      setViewMode("menu"); 
+    }
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="fixed bottom-6 right-6 sm:bottom-8 sm:right-8 z-50 font-sans">
       <AnimatePresence>
